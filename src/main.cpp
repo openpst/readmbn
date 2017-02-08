774abb5e2cb8d8cbbab3255e8aee28ece7208151
Gassan Idriss <ghassani@gmail.com>
2017-02-08 13:00:29 -0800

    license

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 /**
-* LICENSE PLACEHOLDER
+*
+* (c) Gassan Idriss <ghassani@gmail.com>
+* 
+* This file is part of readmbn.
+* 
+* readmbn is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* readmbn is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with readmbn. If not, see <http://www.gnu.org/licenses/>.
 *
 * @file main.cpp
 * @package openpst/readmbn
@@ -7,6 +23,7 @@
 *
 * @author Gassan Idriss <ghassani@gmail.com>
 */
+
 #include <string>
 #include <iostream>
 #include <fstream>
